3af61547d62ac6a35df81e33294b9c8319b96550
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-11-26 14:54:10 -0500

    Start

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
           <li class="media">
             <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
             <div class="media-body">
-              <h5 class="mt-0 mb-1">List-based media object</h5>
+              <h5 class="mt-0 mb-1">Drop Blocks</h5>
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
             </div>
           </li>
