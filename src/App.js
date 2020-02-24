95f79be43b315b3c19477cccdbffefd1bd76328b
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-02-24 11:44:49 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
         <li><a href="#home">Home</a></li>
         <li><a href="#news">News</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a href="#about">WEBSITE CURRENTLY UNDER CONSTRUCTION</a></li>
       </ul>
 
       </div>
