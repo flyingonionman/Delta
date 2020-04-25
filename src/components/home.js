07d8c6ae9667bdd7e2439f43ae98c7fd86328b89
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-04-25 17:37:13 -0400

    some things

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,76 @@
 import React from 'react';
-import '../App.scss';
+import '../css/main.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import * as THREE from "three";
+const OrbitControls = require('three-orbitcontrols');
 
+//Globals
+var camera, scene, renderer,controls,frameId;
+var cube;
 class Home extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  componentDidMount(){
+    this.init();
+    start();
+  }
+
+  init = () =>{
+    var width = window.innerWidth
+    var height = window.innerHeight
+    //ADD SCENE
+
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color( 0x949292  );
+    scene.fog = new THREE.Fog( 0x152238 , 4, 15 );
+
+    // Ground
+
+    var plane = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry( 40, 40 ),
+      new THREE.MeshPhongMaterial( { color: 0x999999, specular: 0x101010 } )
+    );
+    plane.rotation.x = - Math.PI / 2;
+    plane.position.y = - 0.5;
+    scene.add( plane );
+
+    plane.receiveShadow = true;
+
+    //ADD CAMERA
+    camera = new THREE.PerspectiveCamera(
+      35,
+      width / height,
+      1,
+      1000
+    )
+    camera.position.z = 12
+
+    // Lights
+    scene.add( new THREE.HemisphereLight( 0x443333, 0x111122 ) );
+
+    //ADD RENDERER
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setSize(width, height)
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.shadowMap.enabled = true;
+
+    this.mount.appendChild(renderer.domElement)
+
+    //ADD CONTROLS
+    controls = new OrbitControls(camera,renderer.domElement );
+    controls.update();
+
+    //ADD CUBE 
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
+    window.addEventListener('resize', this.handleWindowResize);
+
+  }
+  
   render() {
   return (
 
@@ -12,103 +80,36 @@ class Home extends React.Component {
         <li><a href="#home">Home</a></li>
         <li><a href="#news">News</a></li>
         <li><a href="#contact">Contact</a></li>
-        <li><a href="#about">WEBSITE CURRENTLY UNDER CONSTRUCTION</a></li>
+        <li><a href="#about">//WEBSITE CURRENTLY UNDER CONSTRUCTION//</a></li>
       </ul>
 
       </div>
       <div className="content">
-        <div className="picture">
-             <img src="https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/75199760_2668333133223417_2342838246888701952_n.jpg?_nc_cat=100&_nc_ohc=fvOyMs2mwKgAQm-SisQ-SrZeN-aXTjR0iX60LEut6jK9q3NQsORe9IVwA&_nc_ht=scontent-lga3-1.xx&oh=f3d7471dbe4bf8bcc8cb9c534eca617d&oe=5E85DCCB"/>
-
-        </div>
-        <div className="description">
-
-        <h1>Projects 2020 Spring</h1>
-        
-        <ul class="list-unstyled">
-    
-          <li class="media">
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Machine learning with architecture </h5>
-            </div>
-          </li>
-
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Data science for unfair bail bonds </h5>
-            </div>
-          </li>
-
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Poster design for tax abatement </h5>
-            </div>
-          </li>
-
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Baby monitoring device project</h5>
-              Done as an electrical engineering final project.
-            </div>
-          </li>
-
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">UNION web journal design / programming</h5>
-              Done as an electrical engineering final project.
-            </div>
-          </li>
-        </ul>
-        
-        
-        <h1>Projects 2019 Winter</h1>
-        <ul class="list-unstyled">
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">A small study on BERT</h5>
-              KEK
-            </div>
-          </li>
-          
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Drop Blocks</h5>
-              Implemented Dropblocks in resnet-50 according to <a href="https://arxiv.org/abs/1810.12890">this article</a>. 
-              <a href="https://github.com/ArianaFreitag/cgml-midterm">Source code</a>.
-            </div>
-          </li>
-
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Graphic design portfolio</h5>
-              I know how to use the illustrator a little bit. My sense of color is a bit off but I do what I can do.
-            </div>
-          </li>
-
-          <li class="media">
-            <img src="https://image.flaticon.com/icons/svg/120/120845.svg"   width="64" height="64" class="mr-3" alt="bert"></img>
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">Runescape Hi-score parser</h5>
-              <a href="https://github.com/flyingonionman/ECE-464-1-Databases/tree/master/pset2">Github Link</a>
-
-            </div>
-          </li>
-        </ul>
-
-        </div>
-
+        <div id="canvas" ref={ref => (this.mount = ref)} />
       </div>    
        
     </div>
     );
   }
+}
+
+function start () {
+  if (!frameId) {
+    frameId = requestAnimationFrame(animate)
+  }
+}
+
+function renderScene () {
+  renderer.render(scene, camera)
+}
+
+function animate () {
+  cube.rotation.x += .01
+  cube.rotation.y += .01
+
+  controls.update();
+  renderScene()
+  frameId = window.requestAnimationFrame(animate)
 }
 
 export default Home;
