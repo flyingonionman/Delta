d75fa0884947b7240a42df0918cc332535c6a4ea
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-05-01 13:28:05 -0400

    project description update 2

--- src/components/home.js
+++ src/components/home.js
@@ -53,7 +53,7 @@ class Home extends React.Component {
     super(props);
     this.state = { 
       zoomedin: false ,
-      currproj: 1
+      currproj: 0
     };
   }
   componentDidMount(){
@@ -164,7 +164,7 @@ class Home extends React.Component {
     cube[2] = new THREE.Mesh( geometry2, material2 );
 
     cube[1].position.y = 1;
-    cube[2].position.x = -20;
+    cube[2].position.x = -15;
     cube[1].name = "projects";
     cube[2].name = "about";
 
@@ -247,7 +247,32 @@ class Home extends React.Component {
   }
 
   cycle = () =>{
-    this.setState({currproj: this.state.currproj+1})
+    if( this.state.currproj < 1){
+      this.setState({currproj: this.state.currproj+1})
+
+    }
+    else{
+      this.setState({currproj: 0})
+    }
+
+    if (Math.random() >.5){
+      tween = new TWEEN.Tween( cube[1].rotation)
+      .to({ x:cube[1].rotation.x+2}, 500) 
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .start();
+
+      cube[1].material.emissive.setHSL( Math.random()*.3+.1, .8, .15);
+
+    }
+    else{
+      tween = new TWEEN.Tween( cube[1].rotation)
+      .to({ y:cube[1].rotation.y+2}, 500) 
+      .easing(TWEEN.Easing.Quadratic.Out)
+      .start();
+
+      cube[1].material.emissive.setHSL( Math.random()*.3, .8, .15);
+
+    }
   }
   
   render() {
@@ -266,7 +291,7 @@ class Home extends React.Component {
         {/* <div className="label" >PROJECTS</div> */}
 
         <container className={  this.state.zoomedin ? 'projectappear': 'hidden'}>
-          <Projectlist name={project_list[this.state.currproj-1]}></Projectlist>
+          <Projectlist name={project_list[this.state.currproj]}></Projectlist>
           <button id="cycle" onClick={this.cycle}> Next</button>
 
         </container> 
@@ -349,14 +374,14 @@ function Projectlist(props) {
       architecture to create a system in which you can obtain model files from a png image. We are also using GAN to generate 
       monstrous furnitures that can be plugged back into the model to see how we would actually construct it.
       </p>
-      <button onClick={transition}  id="tomlarch">To galaxy MLARCH</button>
+      <button onClick={() => transition("mlarch")}  id="tomlarch">To MLARCH</button>
       </div>;
   case "datasci":
     return <div>
       <h1>Data science for social good</h1>
       <p>Creating simple but effective visualization for the purpose of helping the bail project.
       </p>
-      <button onClick={transition}  id="tomlarch">To galaxy MLARCH</button>
+      <button onClick={() => transition("datasci")}  id="tomlarch">To Datasci </button>
       </div>;
   default:
     return 0
@@ -365,19 +390,19 @@ function Projectlist(props) {
 
 
  
-function transition(){
-  console.log("pogger")
-  camera.rotation.y = 0;
+function transition(pageurl){
+  
+
   tween = new TWEEN.Tween(camera.position)
-  .to({ y:0,x:0,z:500}, 1500) 
+  .to({ y:0,x:0,z:1000}, 1500) 
   .easing(TWEEN.Easing.Quadratic.Out)
   .start(); 
   
-  tween = new TWEEN.Tween(camera.position)
+  /* tween = new TWEEN.Tween(camera.position)
   .to({ y:0,x:0,z:0}, 1500) 
   .easing(TWEEN.Easing.Quadratic.Out)
-  .start();
-    setTimeout(function(){   window.location = 'mlarch'; }, 3000);
+  .start(); */
+    setTimeout(function(){   window.location = pageurl; }, 3000);
 
 }
 
