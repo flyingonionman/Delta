68e9f5aaaa757e45434e024806d427214faf3588
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2020-05-06 12:52:01 -0400

    new images

--- src/components/mlarch_gallery.js
+++ src/components/mlarch_gallery.js
@@ -64,7 +64,7 @@ class Mlarch extends React.Component {
             <Col><img onClick={() => this.enlarge(9)} src={images["p"+9+".png"]}/></Col>
             <Col><img onClick={() => this.enlarge(10)} src={images["p"+10+".png"]}/></Col>
             <Col><img onClick={() => this.enlarge(11)} src={images["p"+11+".png"]}/></Col>
-            <Col><img onClick={() => this.enlarge(12)} src={images["p"+12+".png"]}/></Col>
+            <Col><img onClick={() => this.enlarge(12)} src={images["p"+12+".gif"]}/></Col>
             <Col><img onClick={() => this.enlarge(13)}src={images["p"+13+".png"]}/></Col>
             <Col><img onClick={() => this.enlarge(14)} src={images["p"+14+".png"]}/></Col>
           </Row>
@@ -128,19 +128,24 @@ function Describe(props){
           return  <p>Generative model in progress. </p>
           break;   
         case 11:
+          return  <p>Quaternion transformation instructions </p>
+          break;   
+        case 11:
           return  <p>A 12 second MS Paint sketch tests the model’s accuracy. </p>
           break; 
         case 12:
-          return  <p>Link to this very website</p>
+          return  <p>Construction of a bench</p>
           break;   
-  
+        case 13:
+          return  <p>Input benches v. output benches</p>
+          break; 
        default:
          return <p>I havne't figured out what to write here yet</p>;
      }
 }
 
 function Welcome(props) {
-  if (props.name==2 || props.name==4){
+  if (props.name==2 || props.name==4 || props.name==12){
     return <img src={images["p"+props.name+".gif"]}></img>;
   }
   else{
